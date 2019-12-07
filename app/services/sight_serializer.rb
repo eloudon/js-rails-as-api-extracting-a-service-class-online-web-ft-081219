86c374bf1b90bc3528cd90d9bd36e361c6288064
Eric Loudon <loudon.eric@gmail.com>
2019-12-07 14:15:03 +0000

    Automatically backed up by Learn

--- app/services/sight_serializer.rb
+++ app/services/sight_serializer.rb
@@ -3,4 +3,11 @@ class SightingSerializer
   def initialize(sightning_object)
     @sightning = sightning_object
   end
+
+  def to_serialized_json
+    @sighting.to_json(:include => {
+    :bird => {:only => [:name, :species]},
+    :location => {:only => [:latitude, :longitude]}
+  }, :except => [:updated_at])
+ end
 end
